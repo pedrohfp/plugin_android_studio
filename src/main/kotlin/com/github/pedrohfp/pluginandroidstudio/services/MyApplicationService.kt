f4f0f1783bac1542a6ccf9b8ad2ea6b20e2d7228
GitHub Action <action@github.com>
2021-04-28 18:01:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/pedrohfp/pluginandroidstudio/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.pedrohfp.pluginandroidstudio.services
+
+import com.github.pedrohfp.pluginandroidstudio.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/pedrohfp/pluginandroidstudio/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pedrohfp/pluginandroidstudio/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pedrohfp.pluginandroidstudio.services
 
+import com.github.pedrohfp.pluginandroidstudio.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
